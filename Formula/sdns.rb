89e205de9a74fe29ce64f60598254718987093a0
GitHub Action <action@github.com>
2020-12-17 20:40:35 +0000

    formatter changes

--- Formula/sdns.rb
+++ Formula/sdns.rb
@@ -24,45 +24,45 @@ class Sdns < Formula
 
   depends_on "go" => :build
 
-  def install
-  end
+  def install; end
 
-  plist_options :startup => false
+  plist_options startup: false
 
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-  <dict>
-    <key>Label</key>
-    <string>#{plist_name}</string>
-    <key>ProgramArguments</key>
-    <array>
-      <string>#{opt_bin}/sdns</string>
-      <string>-config</string>
-      <string>#{etc}/sdns.conf</string>
-    </array>
-    <key>RunAtLoad</key>
-    <true/>
-    <key>KeepAlive</key>
-    <true/>
-    <key>StandardErrorPath</key>
-    <string>#{var}/log/sdns.log</string>
-    <key>StandardOutPath</key>
-    <string>#{var}/log/sdns.log</string>
-    <key>WorkingDirectory</key>
-    <string>#{opt_prefix}</string>
-  </dict>
-</plist>
-
-  EOS
+  def plist
+    <<~EOS
+                <?xml version="1.0" encoding="UTF-8"?>
+            <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+            <plist version="1.0">
+              <dict>
+                <key>Label</key>
+                <string>#{plist_name}</string>
+                <key>ProgramArguments</key>
+                <array>
+                  <string>#{opt_bin}/sdns</string>
+                  <string>-config</string>
+                  <string>#{etc}/sdns.conf</string>
+                </array>
+                <key>RunAtLoad</key>
+                <true/>
+                <key>KeepAlive</key>
+                <true/>
+                <key>StandardErrorPath</key>
+                <string>#{var}/log/sdns.log</string>
+                <key>StandardOutPath</key>
+                <string>#{var}/log/sdns.log</string>
+                <key>WorkingDirectory</key>
+                <string>#{opt_prefix}</string>
+              </dict>
+            </plist>
+      #{'      '}
+    EOS
   end
 
   test do
     fork do
-      exec bin/"sdns", "-config", testpath/"sdns.conf"
+      exec bin / "sdns", "-config", testpath / "sdns.conf"
     end
     sleep(2)
-    assert_predicate testpath/"sdns.conf", :exist?
+    assert_predicate testpath / "sdns.conf", :exist?
   end
 end
