3c6548b066eb5aa2d272321c0e3bffbacd141a40
Yasar Alev <semihalev@gmail.com>
2021-08-20 22:20:55 +0300

    Update sdns.rb

--- Formula/sdns.rb
+++ Formula/sdns.rb
@@ -44,7 +44,7 @@ class Sdns < Formula
 
   def plist
     <<~EOS
-          <?xml version="1.0" encoding="UTF-8"?>
+      <?xml version="1.0" encoding="UTF-8"?>
       <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
       <plist version="1.0">
         <dict>
@@ -68,7 +68,6 @@ class Sdns < Formula
           <string>#{opt_prefix}</string>
         </dict>
       </plist>
-
     EOS
   end
 
