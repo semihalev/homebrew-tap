cf8cee954f64e3ee62bd5274c688a4002ee2f751
GitHub Action <action@github.com>
2023-04-30 11:27:16 +0000

    formatter changes

--- Formula/sdns.rb
+++ Formula/sdns.rb
@@ -59,39 +59,39 @@ class Sdns < Formula
 
   def plist
     <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-  <dict>
-    <key>Label</key>
-    <string>#{plist_name}</string>
-    <key>ProgramArguments</key>
-    <array>
-      <string>#{opt_bin}/sdns</string>
-      <string>-config</string>
-      <string>#{etc}/sdns.conf</string>
-    </array>
-    <key>RunAtLoad</key>
-    <true/>
-    <key>KeepAlive</key>
-    <true/>
-    <key>StandardErrorPath</key>
-    <string>#{var}/log/sdns.log</string>
-    <key>StandardOutPath</key>
-    <string>#{var}/log/sdns.log</string>
-    <key>WorkingDirectory</key>
-    <string>#{opt_prefix}</string>
-  </dict>
-</plist>
+            <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>ProgramArguments</key>
+          <array>
+            <string>#{opt_bin}/sdns</string>
+            <string>-config</string>
+            <string>#{etc}/sdns.conf</string>
+          </array>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>KeepAlive</key>
+          <true/>
+          <key>StandardErrorPath</key>
+          <string>#{var}/log/sdns.log</string>
+          <key>StandardOutPath</key>
+          <string>#{var}/log/sdns.log</string>
+          <key>WorkingDirectory</key>
+          <string>#{opt_prefix}</string>
+        </dict>
+      </plist>
 
     EOS
   end
 
   test do
     fork do
-      exec bin/"sdns", "-config", testpath/"sdns.conf"
+      exec bin / "sdns", "-config", testpath / "sdns.conf"
     end
     sleep(2)
-    assert_predicate testpath/"sdns.conf", :exist?
+    assert_predicate testpath / "sdns.conf", :exist?
   end
 end
